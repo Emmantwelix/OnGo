0b03249d69c34fa230ff89b6cc1de60f567cbb32
emmanuel <emmantwelix@gmail.com>
2026-02-05 20:27:32 -0600

    started flight class

--- app/src/main/java/com/group9/ongo/models/Flight.java
+++ app/src/main/java/com/group9/ongo/models/Flight.java
@@ -1,16 +1,22 @@
 package com.group9.ongo.models;
 
 public class Flight {
+    private static int num_flights = 0; //used to determine flight id
     private String airline;
     private int departTime;
     private int landTime;
     private String destination;
+    private int capacity;
+    private int flightid;
 
-    public Flight(String airline, String destination, int departTime, int landTime) {
+    public Flight(String airline, String destination, int departTime, int landTime, int capacity) {
         this.airline = airline;
         this.destination = destination;
         this.departTime = departTime;
         this.landTime = landTime;
+        this.capacity = capacity;
+        this.flightid = num_flights;
+        num_flights++;
     }
 
     public String getDestination() {
@@ -23,4 +29,8 @@ public class Flight {
 
     public int getDepartTime() { return departTime; }
     public int getLandTime() { return landTime; }
+
+    public int getCapacity () { return  capacity; }
+
+    public int getFlightid () { return flightid; }
 }
--- app/src/main/java/com/group9/ongo/persistence/fake/FakeFlightRepository.java
+++ app/src/main/java/com/group9/ongo/persistence/fake/FakeFlightRepository.java
@@ -8,12 +8,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class FakeFlightRepository implements FlightRepository {
+    //airlines
+    private final String AIR_CANADA = "Air Canada";
+    private final String WESTJET = "Westjet";
+    private final String AIR_TRANSAT = "Air Transat";
+    private final String PORTER_AIRLINES = "Porter Airlines";
+    //places
+    private final String Toronto = "Toronto";
+    private final String MONTREAL = "Montreal";
+    private final String VANCOUVER = "Vancouver";
+    private final String WINNIPEG = "Winnipeg";
+
+    //capacity
+    private final int LARGE_CAPACITY = 200;
+    private final int MEDIUM_CAPACITY = 150;
+    private final int SMALL_CAPACITY = 100;
+
+
+
 
     private final List<Flight> flights = new ArrayList<>();
 
     public FakeFlightRepository() {
-        flights.add(new Flight("Air Canada", "Toronto"));
-        flights.add(new Flight("WestJet", "Montreal"));
+        flights.add(new Flight(AIR_CANADA, VANCOUVER, 1000, 1200, LARGE_CAPACITY));
+        flights.add(new Flight(WESTJET, MONTREAL, 1100, 1300, MEDIUM_CAPACITY));
+        flights.add(new Flight(AIR_TRANSAT, WINNIPEG, 1200, 1400, SMALL_CAPACITY));
+        flights.add(new Flight(PORTER_AIRLINES, Toronto, 1300, 1500, LARGE_CAPACITY));
     }
 
     @Override
