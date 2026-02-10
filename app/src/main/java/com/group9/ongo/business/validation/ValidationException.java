83fe44a0c03302ce3c4cdc40bf905a25f2653cb8
tomas <hagost@myumanitoba.ca>
2026-02-09 20:09:06 -0600

    Added Exceptions

--- app/src/main/java/com/group9/ongo/business/validation/ValidationException.java
+++ app/src/main/java/com/group9/ongo/business/validation/ValidationException.java
@@ -1,4 +1,7 @@
 package com.group9.ongo.business.validation;
 
-public class ValidationException {
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
 }
