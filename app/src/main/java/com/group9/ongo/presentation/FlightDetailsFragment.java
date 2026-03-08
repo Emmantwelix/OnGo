8284d6f9199cf9e8c8a247278db20cb840328e56
Ohang <ohangpatel12@gmail.com>
2026-03-07 18:01:17 -0600

    info. added in the flight detail fragment

--- app/src/main/java/com/group9/ongo/presentation/FlightDetailsFragment.java
+++ app/src/main/java/com/group9/ongo/presentation/FlightDetailsFragment.java
@@ -5,12 +5,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.group9.ongo.R;
+import com.group9.ongo.application.OnGoApp;
+import com.group9.ongo.business.services.FlightService;
+import com.group9.ongo.business.validation.ValidationException;
+import com.group9.ongo.models.Flight;
+
+import java.util.Locale;
 
 public class FlightDetailsFragment extends Fragment {
 
@@ -37,8 +44,37 @@ public class FlightDetailsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_flight_details, container, false);
-        TextView textView = view.findViewById(R.id.text_flight_details);
-        textView.setText("DEtail fragment and the id of the flight " + flightId);
+
+        TextView textFlightId = view.findViewById(R.id.text_flight_id);
+        TextView textAirline = view.findViewById(R.id.text_airline);
+        TextView textRoute = view.findViewById(R.id.text_route);
+        TextView textTimes = view.findViewById(R.id.text_times);
+        TextView textDuration = view.findViewById(R.id.text_duration);
+        TextView textCapacity = view.findViewById(R.id.text_capacity);
+        TextView textPrice = view.findViewById(R.id.text_price);
+
+        FlightService flightService = ((OnGoApp) requireActivity().getApplication()).getFlightService();
+
+        try {
+            Flight flight = flightService.getFlightById(flightId);
+
+            textFlightId.setText(String.format(Locale.ROOT, "Flight Number: %d", flight.getFlightId()));
+            textAirline.setText(String.format("Airline: %s", flight.getAirline()));
+            textRoute.setText(String.format("Route: %s to %s", flight.getOrigin(), flight.getDestination()));
+            textTimes.setText(String.format("Time: %s - %s", flight.getDepartTimeString(), flight.getLandTimeString()));
+
+            int hours = flightService.getDurationHours(flight);
+            int mins = flightService.getDurationRemainingMinutes(flight);
+            textDuration.setText(String.format(Locale.ROOT, "Duration: %dhr %dmin", hours, mins));
+
+            textCapacity.setText(String.format(Locale.ROOT, "Capacity: %d seats", flight.getCapacity()));
+            textPrice.setText(String.format(Locale.ROOT, "Price: $%.2f", flight.getPrice()));
+
+        } catch (ValidationException e) {
+            Toast.makeText(getContext(), "Error loading flight details", Toast.LENGTH_SHORT).show();
+            getParentFragmentManager().popBackStack();
+        }
+
         return view;
     }
 }
