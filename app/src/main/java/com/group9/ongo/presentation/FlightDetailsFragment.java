4e8c405a73e0dd86233aec3cdb99df7baaf0c9fe
Ohang <ohangpatel12@gmail.com>
2026-03-07 21:22:48 -0600

    ui-improvement-2 for flight detail fragment

--- app/src/main/java/com/group9/ongo/presentation/FlightDetailsFragment.java
+++ app/src/main/java/com/group9/ongo/presentation/FlightDetailsFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
 import com.group9.ongo.R;
 import com.group9.ongo.application.OnGoApp;
 import com.group9.ongo.business.services.FlightService;
@@ -47,31 +47,26 @@ public class FlightDetailsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_flight_details, container, false);
 
         // Bind views
-        TextView textDepartTime = view.findViewById(R.id.text_depart_time);
-        TextView textOriginCode = view.findViewById(R.id.text_origin_code);
-        TextView textOriginName = view.findViewById(R.id.text_origin_name);
-        
-        TextView textDuration = view.findViewById(R.id.text_duration);
         TextView textAirline = view.findViewById(R.id.text_airline);
         TextView textFlightId = view.findViewById(R.id.text_flight_id);
-        
-        TextView textArrivalTime = view.findViewById(R.id.text_arrival_time);
+        TextView textOriginCode = view.findViewById(R.id.text_origin_code);
+        TextView textOriginName = view.findViewById(R.id.text_origin_name);
         TextView textDestCode = view.findViewById(R.id.text_dest_code);
         TextView textDestName = view.findViewById(R.id.text_dest_name);
-        
+        TextView textDepartTime = view.findViewById(R.id.text_depart_time);
+        TextView textDuration = view.findViewById(R.id.text_duration);
+        TextView textCapacity = view.findViewById(R.id.text_capacity);
         TextView textPrice = view.findViewById(R.id.text_price);
-        Button btnNext = view.findViewById(R.id.btn_next);
+        MaterialButton btnNext = view.findViewById(R.id.btn_next);
 
         FlightService flightService = ((OnGoApp) requireActivity().getApplication()).getFlightService();
 
         try {
             Flight flight = flightService.getFlightById(flightId);
 
-            // Set times
-            textDepartTime.setText(flight.getDepartTimeString());
-            textArrivalTime.setText(flight.getLandTimeString());
-
-            // Set locations (Using first 3 letters as code placeholder if not already code)
+            textAirline.setText(flight.getAirline());
+            textFlightId.setText(String.format("AC %d", flight.getFlightId())); // Mocking prefix
+            
             String origin = flight.getOrigin();
             String destination = flight.getDestination();
             
@@ -81,16 +76,13 @@ public class FlightDetailsFragment extends Fragment {
             textDestCode.setText(destination.length() >= 3 ? destination.substring(0, 3).toUpperCase() : destination.toUpperCase());
             textDestName.setText(destination);
 
-            // Set Flight Info
-            textAirline.setText(flight.getAirline());
-            textFlightId.setText(String.format("Flight %d", flight.getFlightId()));
+            textDepartTime.setText(flight.getDepartTimeString());
 
-            // Set Duration
             int hours = flightService.getDurationHours(flight);
             int mins = flightService.getDurationRemainingMinutes(flight);
-            textDuration.setText(String.format(Locale.ROOT, "%dhr %dmin", hours, mins));
+            textDuration.setText(String.format(Locale.ROOT, "%dh %dm", hours, mins));
 
-            // Set Price
+            textCapacity.setText(String.valueOf(flight.getCapacity()));
             textPrice.setText(String.format(Locale.ROOT, "$%.2f", flight.getPrice()));
 
             btnNext.setOnClickListener(v -> {
