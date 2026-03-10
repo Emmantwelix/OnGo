00a8c731278f81b1e64fa60d1fd5173f1a7185a6
emmanuel <emmantwelix@gmail.com>
2026-03-09 22:32:11 -0500

    seat test setup

--- app/src/test/java/com/group9/ongo/business/services/SeatServiceTest.java
+++ app/src/test/java/com/group9/ongo/business/services/SeatServiceTest.java
@@ -1,4 +1,33 @@
 package com.group9.ongo.business.services;
 
+import com.group9.ongo.business.services.Implementations.FlightDetailGen;
+import com.group9.ongo.business.services.Implementations.FlightServiceImpl;
+import com.group9.ongo.business.services.Implementations.SeatServiceImplementation;
+import com.group9.ongo.business.services.Interfaces.FlightService;
+import com.group9.ongo.business.services.Interfaces.Generator;
+import com.group9.ongo.business.services.Interfaces.SeatService;
+import com.group9.ongo.persistence.SeatRepository;
+import com.group9.ongo.persistence.fake.FakeFlightRepository;
+import com.group9.ongo.persistence.fake.FakeSeatsRepository;
+
+import org.junit.Before;
+
+import java.util.Random;
+
 public class SeatServiceTest {
+    private FlightService flightService;
+    private SeatService seatService;
+    private Generator fdgen;
+    private SeatRepository seatRepository;
+
+    @Before
+    public void setup() {
+        seatRepository = new FakeSeatsRepository();
+        fdgen = new FlightDetailGen(new Random());
+        seatService = new SeatServiceImplementation(seatRepository);
+        flightService = new FlightServiceImpl(new FakeFlightRepository(), fdgen, seatService);
+    }
+
+
+
 }
